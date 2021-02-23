4f03d042c83f37503ed66d233484017907078466
Joshua-E-Weinstein <joshua.e.weinstein@gmail.com>
2021-02-23 00:53:36 -0500

    Formatting and Renaming

--- Hackathon/Application.py
+++ Hackathon/Application.py
@@ -8,12 +8,13 @@ from StickyNote import StickyNote
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+
         self.master = master
-        # self.pack()
-        button1 = NiceButton("top", master=self.master, text="print hi", command=(lambda: print("hi")))
-        button2 = NiceButton("top", master=self.master, text="close window", command=(lambda: master.destroy()))
+
+        printHi = NiceButton(master=self.master, text="print hi", command=(lambda: self.say_hi()))
+        quitApp = NiceButton(master=self.master, text="close window", command=(lambda: master.destroy()))
         sticky1 = StickyNote(master=self.master, text="Hello", bg="red")
-        map = Map(master=self.master, bg="white", height=300, width=300)
+        googleMap = Map(master=self.master, bg="white", height=300, width=300)
 
     def say_hi(self):
         print("hi there, everyone!")
--- Hackathon/NiceButton.py
+++ Hackathon/NiceButton.py
@@ -2,6 +2,10 @@ import tkinter as tk
 
 
 class NiceButton(tk.Button):
-    def __init__(self, side, master=None, **kwargs):
+    def __init__(self, master=None, **kwargs):
         super().__init__(master, kwargs)
-        self.pack(side=side)
+        self.pack(side="top")
+
+    # Function to pack the button to a frame.
+    def packTo(self, frame):
+        pass
