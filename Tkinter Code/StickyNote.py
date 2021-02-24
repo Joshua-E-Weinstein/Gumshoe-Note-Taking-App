d07b7c6a4ebc99c3f673d8811739f3b362031b45
Joshua-E-Weinstein <joshua.e.weinstein@gmail.com>
2021-02-24 01:10:45 -0500

    Changing to PyQt5

--- Tkinter Code/StickyNote.py
+++ Tkinter Code/StickyNote.py
@@ -3,9 +3,10 @@ import tkinter as tk
 from DragDropMixin import DragDropMixin
 
 
-class StickyNote(DragDropMixin, tk.Label):
+class StickyNote(DragDropMixin, tk.Canvas):
     def __init__(self, master=None, **kwargs):
         super().__init__(master, kwargs)
+        self.create_window(150, 150, window=tk.Entry())
         self.place(x=0, y=0)
 
 
