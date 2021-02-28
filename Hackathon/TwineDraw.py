f4bca2892f2f65f9771440dc138e9aa34db50898
TheFishFlopper <alex@golf18.ca>
2021-02-28 08:41:23 -0500

    Slight visual improvement to twine

--- Hackathon/TwineDraw.py
+++ Hackathon/TwineDraw.py
@@ -7,18 +7,19 @@ import sys
 
 class Twine(QLabel):
 
-    def __init__(self, point1 = QPoint(0,0), point3 = QPoint(50,50), parent=None):
+    def __init__(self, point1=QPoint(0, 0), point3=QPoint(50, 50), parent=None):
         super().__init__(parent)
         self.point1 = point1
         self.point3 = point3
 
         self.getPoint2()
 
-        self.dic = {1: self.point1, 2: self.point2, 3: self.point3}  # Record the three coordinates, which are the starting point, control point and end point
+        self.dic = {1: self.point1, 2: self.point2,
+                    3: self.point3}  # Record the three coordinates, which are the starting point, control point and end point
         self.count = 0
         self.initUI()
         print(self.dic[1])
-    
+
     def getPoint2(self):
         self.point2 = QPoint((self.point1.x() + self.point3.x()) // 2, (self.point1.y() + self.point3.y()) // 2 + 50)
 
@@ -29,7 +30,6 @@ class Twine(QLabel):
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setAttribute(Qt.WA_TransparentForMouseEvents)
         self.show()
-
 
     def paintEvent(self, e):
         pen = QPen()
@@ -43,18 +43,19 @@ class Twine(QLabel):
         self.drawBezierCurve(qp)  # Draw Bezier curve
         qp.end()
 
-
     def drawBezierCurve(self, qp):
         self.clear()
         path = QPainterPath()
         path.moveTo(self.dic[1])
-        path.cubicTo(self.dic[1], self.dic[2], self.dic[3])  # Draw Bezier curve needs to provide three points, starting point, control point and end point
+        path.cubicTo(self.dic[1], self.dic[2], self.dic[
+            3])  # Draw Bezier curve needs to provide three points, starting point, control point and end point
         qp.setRenderHint(QPainter.Antialiasing)
         qp.drawPath(path)
 
+
 class Pin(Draggable):
 
-    def __init__(self, x = 0, y = 0, parent=None):
+    def __init__(self, x=0, y=0, parent=None):
         super().__init__(parent)
         Draggable.passSize(self, x, y, 32, 32)
 
@@ -66,21 +67,23 @@ class Pin(Draggable):
         self.linkage.pos().setX(5)
         secondPin.linkage = self
 
-        self.twine = Twine(self.pos(), self.linkage.pos(), parent)
+        self.twine = Twine(QPoint(self.pos().x() + 16, self.pos().y() + 16), QPoint(self.linkage.pos().x() + 16, self.linkage.pos().y() + 16), parent)
         self.linkage.twine = self.twine
-
 
     def onDrag(self):
         # self.twine.point1 = self.pos()
         # self.twine.point3 = self.linkage.pos()
         # self.twine.getPoint2()
+        pos1 = QPoint(self.pos().x() + 16, self.pos().y() + 16)
+        pos2 = QPoint(self.linkage.pos().x() + 16, self.linkage.pos().y() + 16)
 
-        self.twine.dic[1] = self.pos()
-        self.twine.dic[3] = self.linkage.pos()
-        self.twine.dic[2] = QPoint((self.twine.dic[1].x() + self.twine.dic[3].x()) // 2, (self.twine.dic[1].y() + self.twine.dic[3].y()) // 2 + 50)
+        self.twine.dic[1] = pos1
+        self.twine.dic[3] = pos2
+        self.twine.dic[2] = QPoint((self.twine.dic[1].x() + self.twine.dic[3].x()) // 2,
+                                   (self.twine.dic[1].y() + self.twine.dic[3].y()) // 2 + 50)
 
         self.update()
 
-        #self.twine.getPoint2()
+        # self.twine.getPoint2()
 
-        #self.twine = Twine(self.pos(), self.linkage.pos(), self.parent())
+        # self.twine = Twine(self.pos(), self.linkage.pos(), self.parent())
