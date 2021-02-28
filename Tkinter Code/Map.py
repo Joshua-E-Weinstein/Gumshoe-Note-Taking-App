2b04a33342300945bd8914aceb6d3812cd9e84db
Joshua-E-Weinstein <76671618+Joshua-E-Weinstein@users.noreply.github.com>
2021-02-28 12:27:29 -0500

    Delete Tkinter Code directory

--- Tkinter Code/Map.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-import cefpython3 as cef
-from DragDropMixin import DragDropMixin
-
-
-class Map(DragDropMixin, tk.Canvas):
-    def __init__(self, master=None, **kwargs):
-        super().__init__(master, kwargs)
-        self.pack(side="right")
